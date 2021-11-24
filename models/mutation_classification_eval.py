f3ea4479f0fd7c44274d900e93574d3972e6e833
Anowarul Kabir <akabir0101@gmail.com>
2021-11-24 15:58:39 -0500

    eval and classification model finetune working

--- models/mutation_classification_eval.py
+++ models/mutation_classification_eval.py
@@ -4,25 +4,24 @@ import pandas as pd
 import numpy as np
 import torch
 
-batch_size = 5
+batch_size = 1
 from_col=0
 to_col=1
 label_col=2
-mutation_classification_head="mutation_cls_prediction"
-model_folder = "data/pretrained_models/"
-pretrained_checkpoint = "checkpoint_best.pt"
-dict_path = "data/binarized/train/from/"
+classification_head="mutation_classification"
+model_dir = "outputs/mutation_classification/checkpoints/"
+binarized_path="data/binarized/mutation_classification/"
 
-data =  pd.read_csv("data/tokenized/train.full", header=None)
+data =  pd.read_csv("data/tokenized/val.full", header=None)
 print(data.shape)
 
 split_num=int(len(data) / batch_size)
 batched_data=np.array_split(data, split_num)
 print("Total batches: " + str(len(batched_data)))
 
-roberta_model = RobertaModel.from_pretrained(model_folder, pretrained_checkpoint)
-# model.eval()
-roberta_model.register_classification_head(mutation_classification_head, num_classes=2)
+roberta_model = RobertaModel.from_pretrained(model_name_or_path=model_dir, checkpoint_file="checkpoint_best.pt", data_name_or_path=binarized_path)
+roberta_model.eval()
+#roberta_model.register_classification_head(mutation_classification_head, num_classes=2)
 
 label_fn = lambda label: roberta_model.task.label_dictionary.string(
     [label + roberta_model.task.label_dictionary.nspecial]
@@ -36,9 +35,9 @@ for count, batch_df in enumerate(batched_data):
         batch=collate_tokens([torch.cat((roberta_model.encode(tokens[from_col], tokens[to_col]), torch.ones(512, dtype = torch.long)))[:512]
             for tokens in batch_df.itertuples(index=False)], pad_idx=1)
         # print(batch)
-        logprobs = label_fn(roberta_model.predict(mutation_classification_head, batch).argmax().item())      
+        logprobs = label_fn(roberta_model.predict(classification_head, batch).argmax().item())      
         print(logprobs)
-    break
+   # break
 
 # tokens = model.encode('▁MI VF VR FN SS')
 # print(tokens)
--- models/mutation_classification_finetune.sh
+++ models/mutation_classification_finetune.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/sh
 
-#SBATCH --job-name=PMA_proberta
+#SBATCH --job-name=mutation_classification
 #SBATCH --qos=csqos
-#SBATCH --output=/scratch/akabir4/mutation_analysis_by_PRoBERTa/outputs/argo_logs/PMA_proberta-%N-%j.output
-#SBATCH --error=/scratch/akabir4/mutation_analysis_by_PRoBERTa/outputs/argo_logs/PMA_proberta-%N-%j.error
+#SBATCH --output=/scratch/akabir4/mutation_analysis_by_PRoBERTa/outputs/argo_logs/mutation_classification-%N-%j.output
+#SBATCH --error=/scratch/akabir4/mutation_analysis_by_PRoBERTa/outputs/argo_logs/mutation_classification-%N-%j.error
 #SBATCH --mail-user=<akabir4@gmu.edu>
 #SBATCH --mail-type=BEGIN,END,FAIL
 
@@ -11,10 +11,13 @@
 #SBATCH --partition=gpuq
 #SBATCH --mem=16000MB
 
-PREFIX="pma"
+JOB="mutation_classification"
+DATA_DIR=data/binarized/$JOB
+OUTPUT_DIR=outputs/$JOB
+CHECKPOINT_DIR="$OUTPUT_DIR/checkpoints"
+mkdir -p $CHECKPOINT_DIR
+
 NUM_GPUS=1
-OUTPUT_DIR=outputs/pma_prediction
-DATA_DIR=data/binarized/train
 
 ENCODER_EMBED_DIM=768
 ENCODER_LAYERS=5
@@ -25,31 +28,23 @@ MAX_SENTENCES=32
 UPDATE_FREQ=64
 
 NUM_CLASSES=2
-PATIENCE=3
-ROBERTA_PATH=data/pretrained_models/checkpoint_best.pt
-
-CLASSIFICATION_HEAD="protein_mutation_analysis"
-
-# USE_CLS="<cls>"
+PATIENCE=5
 
 TOKENS_PER_SAMPLE=512
 MAX_POSITIONS=512
 
-# BATCH_SIZE=$((MAX_SENTENCES*UPDATE_FREQ*NUM_GPUS))
-
+PREFIX=$JOB
 PREFIX="$PREFIX.DIM_$ENCODER_EMBED_DIM.LAYERS_$ENCODER_LAYERS"
 PREFIX="$PREFIX.UPDATES_$TOTAL_UPDATES.WARMUP_$WARMUP_UPDATES"
 PREFIX="$PREFIX.LR_$PEAK_LR.BATCH_$MAX_SENTENCES.PATIENCE_$PATIENCE"
+LOG_FILE="$OUTPUT_DIR/$PREFIX.log"
 
-CHECKPOINT_DIR="$OUTPUT_DIR/$PREFIX/checkpoints"
-LOG_FILE="$OUTPUT_DIR/$PREFIX/$PREFIX.log"
-
-mkdir -p $CHECKPOINT_DIR
+ROBERTA_PATH=data/pretrained_models/checkpoint_best.pt
+CLASSIFICATION_HEAD=$JOB
 
 CUDA_VISIBLE_DEVICES=0 fairseq-train $DATA_DIR \
     --max-positions $MAX_POSITIONS --batch-size $MAX_SENTENCES  \
     --arch roberta_base --task sentence_prediction \
-    --bpe sentencepiece \
     --classification-head-name $CLASSIFICATION_HEAD \
     --restore-file $ROBERTA_PATH --reset-optimizer --reset-dataloader --reset-meters \
     --init-token 0 --separator-token 2 \
